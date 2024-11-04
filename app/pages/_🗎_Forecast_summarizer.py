1ee5feb3f5b5ee0a0be0db3acc38f78e655cc8c1
Witchaz <Jengkug23@gmail.com>
2024-11-04 19:01:06 +0700

    feat : b2b forecasting summarize

--- app/pages/_🗎_Forecast_summarizer.py
+++ app/pages/_🗎_Forecast_summarizer.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd 
+import statistics 
 st.set_page_config(
     page_title="Forecasting summarizer",
     page_icon="📊",
@@ -9,23 +10,33 @@ st.sidebar.markdown("ช่วยในการสรุปผลการท�
 
 
 
-b2c_file = st.file_uploader("B2C forecast")
+b2c_file = st.file_uploader("B2C forecast",type="csv")
 if b2c_file :
-    df = pd.read_csv(b2c_file)
+    b2c_df = pd.read_csv(b2c_file)
     # ลบคอลัมน์ 'Category' ออกเพราะไม่ได้ใช้ในการคำนวณ
-    df = df.drop(columns=["Category"])
+    b2c_df = b2c_df.drop(columns=["Category"])
 
     # แยกชื่อคอลัมน์เพื่อสร้าง MultiIndex โดยแบ่งเป็น 'City' และ 'Product'
     new_columns = pd.MultiIndex.from_tuples(
-        [col.split('-', 1) for col in df.columns], names=["City", "Product"]
+        [col.split('-', 1) for col in b2c_df.columns], names=["City", "Product"]
     )
-    df.columns = new_columns
+    b2c_df.columns = new_columns
     city_options = new_columns.get_level_values(0).drop_duplicates(keep='first')
     options = st.multiselect("please select city",city_options,default=city_options[0])
     try:
-        result = df[options].agg(['mean', 'max', 'min'])
-        st.dataframe(result.T,
-                use_container_width= True,
-                column_config={})
+        result = b2c_df[options].agg(['mean', 'max', 'min','std'])
+        result_t = result.T
+        result_t=  result_t.astype({'mean':'int64','std':'int64'})
+        st.dataframe(result_t,
+                use_container_width= True)
     except:
         st.error("Error : Please select at least one city.")
+
+b2b_file = st.file_uploader("B2B forecast",type="csv")
+if b2b_file:
+    b2b_df = pd.read_csv(b2b_file)
+    b2b_df.drop(columns=["Day"],inplace=True)
+    result = b2b_df.agg(['mean','max','min','std'])
+    result_t = result.T
+    result_t = result_t.astype({'mean':'int64','std':'int64'})
+    st.dataframe(result_t,use_container_width=True)
