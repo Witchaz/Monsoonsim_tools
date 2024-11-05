ec44781637511e009755519816a148acc918ff58
Witchaz <Jengkug23@gmail.com>
2024-11-05 07:18:31 +0700

    feat : Blacklist reatail or B2B Client

--- app/pages/_🚚_distance_matrix.py
+++ app/pages/_🚚_distance_matrix.py
@@ -3,28 +3,46 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-def distance_matrix(distance_file):
-    df = pd.read_csv(distance_file)    
-    df.columns = ['City_Pair', 'Distance']
+def distance_matrix(distance_file,b2b_file):
+    distance_df = pd.read_csv(distance_file)
+    b2b_df = pd.read_csv(b2b_file)
+    distance_df.columns = ['City_Pair', 'Distance']
+
+    b2b_df.set_index("Category", inplace=True)
+    # Iterate over the DataFrame to generate the formatted strings
+    
+    for index, row in b2b_df.iterrows():
+        for column, distance in row.items():
+            distance_df.loc[len(distance_df.index)] = [f"{index} - {column}",distance]
 
     # Extract unique cities from City_Pair column
-    city_pairs = df['City_Pair'].str.split(' - ', expand=True)
+    city_pairs = distance_df['City_Pair'].str.split(' - ', expand=True)
     cities = pd.unique(city_pairs.values.ravel())
-
+    
     # Initialize an empty distance matrix with cities as both row and column indices
     distance_matrix = pd.DataFrame(np.inf, index=cities, columns=cities)
 
     # Populate the matrix with distances
-    for _, row in df.iterrows():
+    for _, row in distance_df.iterrows():
         city1, city2 = row['City_Pair'].split(' - ')
         distance = row['Distance']
         # Set the distance for both [city1, city2] and [city2, city1] as the matrix is symmetric
         distance_matrix.loc[city1, city2] = distance
         distance_matrix.loc[city2, city1] = distance
+        
+    distance_matrix.drop([""],axis=0,inplace=True)
+    distance_matrix.drop([""],axis=1,inplace=True)
+
+    city_options = distance_matrix.columns
+    
+    options = st.multiselect("Blacklist city",city_options)
+
+    distance_matrix.drop(options,axis=0,inplace=True)
+    distance_matrix.drop(options,axis=1,inplace=True)
 
     # Fill diagonal with zeros, as distance from a city to itself is zero
     np.fill_diagonal(distance_matrix.values, 0)
-    distance_matrix = distance_matrix.iloc[0:-1,0:-1]
+    
     # Apply color gradient from green (low) to red (high) using 'RdYlGn'
     styled_matrix = distance_matrix.style.background_gradient(cmap="RdYlGn_r", vmin=0,vmax=2000, axis=1)
 
@@ -33,12 +51,13 @@ def distance_matrix(distance_file):
 
 st.set_page_config(
     page_title="Distance Matrix",
-    page_icon="📦",
+    page_icon="🚚",
 )
 
 st.sidebar.header("Distance Matrix")
 st.sidebar.markdown("Visualize the distances between each pair of nodes")
 
-distance_file = st.file_uploader("Distance (.csv)", type="csv")
-if distance_file:
-    distance_matrix(distance_file)
+distance_file = st.file_uploader("Retail Distance (.csv)", type="csv")
+b2b_file = st.file_uploader("B2B distance (.csv)",type="csv")
+if distance_file and b2b_file:
+    distance_matrix(distance_file,b2b_file)
